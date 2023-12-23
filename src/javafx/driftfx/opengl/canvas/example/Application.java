31a6382c756776e343239f20280b28db4e552a72
Pavel Ruban <job.pavelruban@gmail.com>
2023-12-23 04:16:42 +0000

    Clean code a bit

--- src/javafx/driftfx/opengl/canvas/example/Application.java
+++ src/javafx/driftfx/opengl/canvas/example/Application.java
@@ -2,34 +2,7 @@ package javafx.driftfx.opengl.canvas.example;
 
 import javafx.driftfx.opengl.canvas.example.fx.JFXWindow;
 import javafx.driftfx.opengl.canvas.example.rendering.RenderLoop;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
-import javafx.stage.Stage;
-import org.eclipse.fx.drift.*;
-import org.lwjgl.opengl.GL11;
-import org.lwjgl.opengl.GL30;
-import org.lwjgl.opengl.GL32;
-import org.lwjgl.opengl.GLCapabilities;
-
 import java.lang.Thread.UncaughtExceptionHandler;
-
-import static java.lang.Thread.sleep;
-import static org.lwjgl.opengl.GL11.glClearColor;
-
-import org.lwjgl.*;
-import org.lwjgl.glfw.*;
-import org.lwjgl.opengl.*;
-import org.lwjgl.system.*;
-
-import java.nio.*;
-
-import static org.lwjgl.glfw.Callbacks.*;
-import static org.lwjgl.glfw.GLFW.*;
-import static org.lwjgl.opengl.GL11.*;
-import static org.lwjgl.system.MemoryStack.*;
-import static org.lwjgl.system.MemoryUtil.*;
 
 /**
  * This class controls all aspects of the application's execution
